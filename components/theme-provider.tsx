d1be165461fe674aca2bb5bb81c09f8d3c0210fb
HudsCG <contatoclimbergoat@gmail.com>
2025-06-03 08:03:02 -0300

    commit 18

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,11 +1,25 @@
-'use client'
+"use client"
 
-import * as React from 'react'
-import {
-  ThemeProvider as NextThemesProvider,
-  type ThemeProviderProps,
-} from 'next-themes'
+import type * as React from "react"
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+type Theme = "dark" | "light" | "system"
+
+type ThemeProviderProps = {
+  children: React.ReactNode
+  defaultTheme?: Theme
+  attribute?: string
+  enableSystem?: boolean
+  disableTransitionOnChange?: boolean
+}
+
+export function ThemeProvider({
+  children,
+  defaultTheme = "light",
+  attribute = "class",
+  enableSystem = true,
+  disableTransitionOnChange = false,
+}: ThemeProviderProps) {
+  // Como estamos usando principalmente o tema light, vamos simplificar
+  // e apenas renderizar os filhos sem lógica de tema
+  return <>{children}</>
 }
